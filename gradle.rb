b40b800e8da713b92ac85df8a3fa5d65179b4f88
Jonathan Huang <jonathanhuang@xrex.io>
2021-03-26 18:14:35 +0800

    Add gradle.rb for version 5.6.4

---
+++ gradle.rb
@@ -0,0 +1,43 @@
+class Gradle < Formula
+  desc "Open-source build automation tool based on the Groovy and Kotlin DSL"
+  homepage "https://www.gradle.org/"
+  url "https://services.gradle.org/distributions/gradle-5.6.4-all.zip"
+  sha256 "abc10bcedb58806e8654210f96031db541bcd2d6fc3161e81cb0572d6a15e821"
+  license "Apache-2.0"
+
+  livecheck do
+    url "https://services.gradle.org/distributions/"
+    regex(/href=.*?gradle[._-]v?(\d+(?:\.\d+)+)-all\.(?:[tz])/i)
+  end
+
+  bottle :unneeded
+
+  # gradle currently does not support Java 16
+  if Hardware::CPU.arm?
+    depends_on "openjdk@11"
+  else
+    depends_on "openjdk"
+  end
+
+  def install
+    rm_f Dir["bin/*.bat"]
+    libexec.install %w[bin docs lib src]
+    env = if Hardware::CPU.arm?
+      Language::Java.overridable_java_home_env("11")
+    else
+      Language::Java.overridable_java_home_env
+    end
+    (bin/"gradle").write_env_script libexec/"bin/gradle", env
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/gradle --version")
+
+    (testpath/"settings.gradle").write ""
+    (testpath/"build.gradle").write <<~EOS
+      println "gradle works!"
+    EOS
+    gradle_output = shell_output("#{bin}/gradle build --no-daemon")
+    assert_includes gradle_output, "gradle works!"
+  end
+end
